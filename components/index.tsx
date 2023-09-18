2c3435f5a4776f1a5c3d8da69f19feb9dc496872
gift56 <efeasiughu@gmail.com>
2023-09-18 17:37:26 -0400

    done working on hero section

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,27 @@
 import CustomizeButton from "./CustomizeButton";
 import Footer from "./Footer";
+import Faqs from "./LandingpageContent/Faqs";
 import Hero from "./LandingpageContent/Hero";
+import Introduction from "./LandingpageContent/Introduction";
+import Judging from "./LandingpageContent/Judging";
+import Price from "./LandingpageContent/Price";
+import Privacy from "./LandingpageContent/Privacy";
+import Rules from "./LandingpageContent/Rules";
+import Sponsors from "./LandingpageContent/Sponsors";
+import Timeline from "./LandingpageContent/Timeline";
 import Navbar from "./Navbar";
 
-export { CustomizeButton, Hero, Navbar, Footer };
+export {
+  CustomizeButton,
+  Hero,
+  Navbar,
+  Footer,
+  Introduction,
+  Rules,
+  Judging,
+  Faqs,
+  Timeline,
+  Price,
+  Sponsors,
+  Privacy,
+};
