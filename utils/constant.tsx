c62c7fcb13b245ce55bd73fd03c209ee946c2b49
gift56 <efeasiughu@gmail.com>
2023-09-18 15:16:12 -0400

    working on types

--- utils/constant.tsx
+++ utils/constant.tsx
@@ -35,3 +35,22 @@ export const footerLinks = [
     ],
   },
 ];
+
+export const socialIcons = [
+  {
+    href: "https://www.instagram.com/getlinked.ai",
+    icon: "/icon/instagramIcon.svg",
+  },
+  {
+    href: "https://twitter.com/getLinkedai",
+    icon: "/icon/xIcon.svg",
+  },
+  {
+    href: "https://web.facebook.com/getLinkedai",
+    icon: "/icon/facebookIcon.svg",
+  },
+  {
+    href: "https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Aorganization%3A97918625&keywords=getlinked.ai&origin=RICH_QUERY_SUGGESTION&position=0&searchId=1628d90e-971c-4eeb-b099-7284b7054076&sid=n_h",
+    icon: "/icon/linkedinIcon.svg",
+  },
+];
