143040c21ef009c7bc279c626b96a771d009c331
gift56 <efeasiughu@gmail.com>
2023-09-18 14:59:54 -0400

    done folder structure and the likes time to get to the main work

--- components/index.tsx
+++ components/index.tsx
@@ -1,1 +1,3 @@
-export {}
+import CustomizeButton from "./CustomizeButton";
+
+export { CustomizeButton };
--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,8 @@
-export interface FormValue{
-    
+export interface FormValue {
+  teamName: String;
+  phone: String;
+  email: String;
+  topic: String;
+  category: String;
+  size: String;
 }
