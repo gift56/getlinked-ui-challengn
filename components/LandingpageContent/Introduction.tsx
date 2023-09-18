29839ceec65fd48b2fbcb3565e15182819733d70
gift56 <efeasiughu@gmail.com>
2023-09-18 17:51:09 -0400

    moving to the next

--- components/LandingpageContent/Introduction.tsx
+++ components/LandingpageContent/Introduction.tsx
@@ -17,13 +17,25 @@ const Introduction = () => {
             />
           </div>
           <div className="w-full tab:flex-1 flex flex-col gap-1">
-            <h2 className="text-xl font-bold md:text-2xl text-center tab:text-start tab:text-3xl font-clash">
+            <motion.h2
+              initial={{ opacity: 0, x: 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ type: "spring", duration: 2 }}
+              viewport={{ once: true }}
+              className="text-xl font-bold md:text-2xl text-center tab:text-start tab:text-3xl font-clash"
+            >
               Introduction to getlinked{" "}
               <span className="font-clash text-primary block">
                 tech Hackathon 1.0
               </span>
-            </h2>
-            <p className="text-center text-sm  font-normal tab:text-start">
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", duration: 2 }}
+              viewport={{ once: true }}
+              className="text-center text-sm  font-normal tab:text-start"
+            >
               Our tech hackathon is a melting pot of visionaries, and its
               purpose is as clear as day: to shape the future. Whether you're a
               coding genius, a design maverick, or a concept wizard, you'll have
@@ -31,7 +43,7 @@ const Introduction = () => {
               real-world problems, pushing the boundaries of technology, and
               creating solutions that can change the world, that's what we're
               all about!
-            </p>
+            </motion.p>
           </div>
         </div>
       </div>
