bd585e2a2661dacbc145d06e355f2ed9ed7d84df
gift56 <efeasiughu@gmail.com>
2023-09-18 16:34:31 -0400

    working on hero page

--- components/LandingpageContent/Hero.tsx
+++ components/LandingpageContent/Hero.tsx
@@ -2,8 +2,10 @@ const Hero = () => {
   return (
     <section className="w-full h-screen lg:overflow-hidden">
       <div className="container">
-        <div className="w-full flex items-center justify-center md:justify-end md:item-end flex-col">
-          <h2 className="">Igniting a Revolution in HR Innovation</h2>
+        <div className="w-full flex items-center justify-center md:justify-end md:items-end flex-col pt-4">
+          <h2 className="text-base font-bold">
+            <i>Igniting a Revolution in HR Innovation</i>
+          </h2>
         </div>
         <div className="w-full flex items-center justify-between gap-6 flex-col md:flex-row">
           <div className="w-full md:flex-1 h-full flex items-center justify-start"></div>
