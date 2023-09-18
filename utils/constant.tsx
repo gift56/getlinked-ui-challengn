6fd23d14a736e10c013b72f19c9f908a191555a5
gift56 <efeasiughu@gmail.com>
2023-09-18 15:05:32 -0400

    working on types

---
+++ utils/constant.tsx
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    href: "timeline",
+    text: "Timeline",
+  },
+  {
+    href: "overview",
+    text: "Overview",
+  },
+  {
+    href: "faq",
+    text: "FAQs",
+  },
+  {
+    href: "/contact",
+    text: "Contact",
+  },
+];
