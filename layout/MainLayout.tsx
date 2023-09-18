dbb4997ea119d4258e3b0d37f156f265227eaa65
gift56 <efeasiughu@gmail.com>
2023-09-18 12:57:55 -0400

    working on getlinked project

--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -1,5 +1,19 @@
-const MainLayout = () => {
-  return <div>MainLayout</div>;
+import { ReactNode } from "react";
+import Head from "next/head";
+
+interface LayoutProps {
+  children: ReactNode;
+  pagetitle: String;
+}
+
+const MainLayout = ({ children, pagetitle }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>getlinked</title>
+      </Head>
+    </>
+  );
 };
 
 export default MainLayout;
