6c61cfedeaf44ec2d228208bce6b7b7019708520
gift56 <efeasiughu@gmail.com>
2023-09-19 12:27:07 -0400

    moving to the next

--- components/LandingpageContent/Privacy.tsx
+++ components/LandingpageContent/Privacy.tsx
@@ -3,7 +3,22 @@ import { motion } from "framer-motion";
 const Privacy = () => {
   return (
     <section id="overview" className="w-full py-10">
-      Privacy
+      <div className="container">
+        <div className="w-full flex flex-col items-center justify-center gap-8 tab:flex-row tab:justify-between tab:items-start">
+          <div className="w-full"></div>
+          <div className="w-full">
+            <motion.img
+              initial={{ opacity: 0, scale: 0.5 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ type: "spring", duration: 2 }}
+              viewport={{ once: true }}
+              src="/img/privacyImg.png"
+              alt="sponsors"
+              className="w-full object-contain"
+            />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
