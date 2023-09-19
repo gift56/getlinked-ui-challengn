9fb70e65c24413c6ce1f8793292cba9939a4725e
gift56 <efeasiughu@gmail.com>
2023-09-19 08:59:50 -0400

    moving to the next

--- components/LandingpageContent/Timeline.tsx
+++ components/LandingpageContent/Timeline.tsx
@@ -1,8 +1,32 @@
+import { motion } from "framer-motion";
+
 const Timeline = () => {
   return (
     <section className="w-full py-10">
-      <div className="container">
-        <div className="w-full"></div>
+      <div className="container overflow-hidden">
+        <div className="w-full flex flex-col items-center justify-center">
+          <div className="w-full tab:flex-1 flex flex-col gap-5">
+            <motion.h2
+              initial={{ opacity: 0, y: -50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ type: "spring", duration: 2 }}
+              viewport={{ once: true }}
+              className="text-xl font-bold md:text-2xl text-center tab:text-start tab:text-3xl font-clash"
+            >
+              Timeline
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", duration: 2 }}
+              viewport={{ once: true }}
+              className="text-center text-sm  font-normal tab:text-start"
+            >
+              Here is the breakdown of the time we <br className="hidden tab:block" /> anticipate using for the
+              upcoming event.
+            </motion.p>
+          </div>
+        </div>
       </div>
     </section>
   );
