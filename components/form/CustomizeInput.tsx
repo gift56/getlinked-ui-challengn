79be221dde1b3f8a6e804471fce6bee3cb28c3c3
gift56 <efeasiughu@gmail.com>
2023-09-19 15:12:22 -0400

    working on signup page

---
+++ components/form/CustomizeInput.tsx
@@ -0,0 +1,61 @@
+import React, { ChangeEvent, FocusEvent, InputHTMLAttributes } from "react";
+
+interface CustomizeInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  htmlFor?: string;
+  showLabel?: boolean;
+  defaultChecked?: boolean;
+  id?: string;
+  label?: string;
+  className?: string;
+  labelClassName?: string;
+  inputClassName?: string;
+  errorClass?: string;
+  error?: any;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (event: FocusEvent<HTMLInputElement>) => void;
+  containerClass?: string;
+}
+
+const CustomizeInput = ({
+  htmlFor,
+  showLabel,
+  id,
+  label,
+  className,
+  labelClassName,
+  inputClassName,
+  error,
+  errorClass,
+  onChange,
+  onBlur,
+  accept,
+  containerClass,
+  defaultChecked,
+  ...props
+}: CustomizeInputProps) => {
+  return (
+    <div className={`flex flex-col gap-1 w-full ${containerClass}`}>
+      {showLabel === false && (
+        <label htmlFor={htmlFor} className={labelClassName}>
+          {label}
+        </label>
+      )}
+      <div className={`flex flex-col gap-1 w-full h-full ${inputClassName}`}>
+        <input
+          {...props}
+          className={`${className} ${error ? "border !border-red-400" : ""}`}
+          id={id}
+          onBlur={onBlur}
+          onChange={onChange}
+          accept={accept}
+          checked={defaultChecked}
+        />
+        {error && (
+          <p className={`text-xs text-red-600 ${errorClass}`}>{error}</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CustomizeInput;
---
+++ components/form/CustomizeSelect.tsx
@@ -0,0 +1,56 @@
+import { ChangeEvent, FocusEvent, SelectHTMLAttributes } from "react";
+
+interface CustomizeSelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  htmlFor?: string;
+  showLabel?: boolean;
+  id?: string;
+  label?: string;
+  className?: string;
+  labelClassName?: string;
+  inputClassName?: string;
+  errorClass?: string;
+  error?: any;
+  onChange?: (event: ChangeEvent<HTMLSelectElement>) => void | any;
+  onBlur?: (event: FocusEvent<HTMLSelectElement>) => void;
+  containerClass?: string;
+}
+
+const CustomizeSelect = ({
+  htmlFor,
+  showLabel,
+  id,
+  label,
+  className,
+  labelClassName,
+  inputClassName,
+  error,
+  errorClass,
+  onChange,
+  onBlur,
+  containerClass,
+  ...props
+}: CustomizeSelectProps) => {
+  return (
+    <div className={`flex flex-col gap-1 w-full ${containerClass}`}>
+      {showLabel === false && (
+        <label htmlFor={htmlFor} className={labelClassName}>
+          {label}
+        </label>
+      )}
+      <div className={`flex flex-col gap-1 w-full h-full ${inputClassName}`}>
+        <select
+          {...props}
+          className={`${className} ${error ? "border border-red-400" : ""}`}
+          id={id}
+          onBlur={onBlur}
+          onChange={onChange}
+        />
+        {error && (
+          <p className={`text-xs text-red-600 ${errorClass}`}>{error}</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CustomizeSelect;
