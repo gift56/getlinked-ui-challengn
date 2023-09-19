bac34d50ecd62d94aba9f40f520767aa48336e58
gift56 <efeasiughu@gmail.com>
2023-09-19 15:51:51 -0400

    working on signup page

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,5 +1,21 @@
+import MainLayout from "@/layout/MainLayout";
+import Head from "next/head";
+
 const Registerpage = () => {
-  return <div>Registerpage</div>;
+  return (
+    <>
+      <Head>
+        <title>Getlinked - Register</title>
+        <meta
+          name="description"
+          content="Participate in getlinked tech Hackathon 2023 stand a chance to win a Big prize"
+        />
+      </Head>
+      <section className="w-full tab:h-screen tab:overflow-hidden">
+        <div className="container"></div>
+      </section>
+    </>
+  );
 };
 
 export default Registerpage;
