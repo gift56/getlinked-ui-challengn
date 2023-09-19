99f9c398d4406bb38b86fe069e712b8d081c940b
gift56 <efeasiughu@gmail.com>
2023-09-19 13:03:18 -0400

    working on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <footer className="w-full py-14">Footer</footer>;
+  return (
+    <footer className="w-full py-14 bg-footerBg">
+      <div className="container">
+        <div className="w-full flex items-start justify-start tab:justify-between"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
