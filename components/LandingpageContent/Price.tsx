9d1c77720ee98e6cd8b7ad329be7719e6e6ed22c
gift56 <efeasiughu@gmail.com>
2023-09-19 11:24:13 -0400

    moving to the next

--- components/LandingpageContent/Price.tsx
+++ components/LandingpageContent/Price.tsx
@@ -1,7 +1,11 @@
 const Price = () => {
-  return <section className="w-full py-10">
-    
-  </section>;
+  return (
+    <section className="w-full py-10 bg-pricebg">
+      <div className="container">
+        <div className="w-full flex flex-col items-center justify-center gap-8"></div>
+      </div>
+    </section>
+  );
 };
 
 export default Price;
