5b2fa6fd7d13e1e575835430fdce4e4ee2971f09
gift56 <efeasiughu@gmail.com>
2023-09-19 09:00:37 -0400

    moving to the next

--- components/LandingpageContent/Timeline.tsx
+++ components/LandingpageContent/Timeline.tsx
@@ -5,13 +5,13 @@ const Timeline = () => {
     <section className="w-full py-10">
       <div className="container overflow-hidden">
         <div className="w-full flex flex-col items-center justify-center">
-          <div className="w-full tab:flex-1 flex flex-col gap-5">
+          <div className="flex flex-col gap-5">
             <motion.h2
               initial={{ opacity: 0, y: -50 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ type: "spring", duration: 2 }}
               viewport={{ once: true }}
-              className="text-xl font-bold md:text-2xl text-center tab:text-start tab:text-3xl font-clash"
+              className="text-xl font-bold md:text-2xl text-center tab:text-3xl font-clash"
             >
               Timeline
             </motion.h2>
@@ -20,9 +20,10 @@ const Timeline = () => {
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ type: "spring", duration: 2 }}
               viewport={{ once: true }}
-              className="text-center text-sm  font-normal tab:text-start"
+              className="text-center text-sm font-normal tab:text-start"
             >
-              Here is the breakdown of the time we <br className="hidden tab:block" /> anticipate using for the
+              Here is the breakdown of the time we{" "}
+              <br className="hidden tab:block" /> anticipate using for the
               upcoming event.
             </motion.p>
           </div>
