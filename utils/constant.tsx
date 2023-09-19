8f5326bf989afe5c3f2604613dee151f76ad1fd1
gift56 <efeasiughu@gmail.com>
2023-09-19 07:45:22 -0400

    moving to the next

--- utils/constant.tsx
+++ utils/constant.tsx
@@ -54,3 +54,26 @@ export const socialIcons = [
     icon: "/icon/linkedinIcon.svg",
   },
 ];
+
+export const judgingCriteria = [
+  {
+    headline: "Innovation and Creativity: ",
+    content: "Evaluate the uniqueness and creativity of the solution. Consider whether it addresses a real-world problem in a novel way or introduces innovative features.",
+  },
+  {
+    headline: "Functionality: ",
+    content: "Assess how well the solution works. Does it perform its intended functions effectively and without major issues? Judges would consider the completeness and robustness of the solution. ",
+  },
+  {
+    headline: "Impact and Relevance: ",
+    content: "Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.",
+  },
+  {
+    headline: "Technical Complexity: ",
+    content: "Evaluate the technical sophistication of the solution. Judges would consider the complexity of the code, the use of advanced technologies or algorithms, and the scalability of the solution.",
+  },
+  {
+    headline: "Adherence to Hackathon Rules: ",
+    content: "Judges will Ensure that the team adhered to the rules and guidelines of the hackathon, including deadlines, use of specific technologies or APIs, and any other competition-specific requirements.",
+  },
+];
