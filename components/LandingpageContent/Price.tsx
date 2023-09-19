27e3f7572b09df8f94bde7de3fd910a92abf42fb
gift56 <efeasiughu@gmail.com>
2023-09-19 11:24:57 -0400

    moving to the next

--- components/LandingpageContent/Price.tsx
+++ components/LandingpageContent/Price.tsx
@@ -1,8 +1,12 @@
+import { motion } from "framer-motion";
+
 const Price = () => {
   return (
     <section className="w-full py-10 bg-pricebg">
       <div className="container">
-        <div className="w-full flex flex-col items-center justify-center gap-8"></div>
+        <div className="w-full flex flex-col items-center justify-center gap-8">
+
+        </div>
       </div>
     </section>
   );
