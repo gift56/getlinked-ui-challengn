fdf47e22be5c156df275fbea9ef0a9d86b2a169b
gift56 <efeasiughu@gmail.com>
2023-09-19 12:25:15 -0400

    moving to the next

--- components/LandingpageContent/Privacy.tsx
+++ components/LandingpageContent/Privacy.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 const Privacy = () => {
   return (
-    <section className="w-full py-10">
+    <section id="overview" className="w-full py-10">
       Privacy
     </section>
   );
