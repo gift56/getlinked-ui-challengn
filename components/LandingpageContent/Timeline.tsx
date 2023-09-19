fcdb4e97c943f6c4cff3efaa141a302464ad2aad
gift56 <efeasiughu@gmail.com>
2023-09-19 10:54:49 -0400

    moving to the next

--- components/LandingpageContent/Timeline.tsx
+++ components/LandingpageContent/Timeline.tsx
@@ -27,9 +27,10 @@ const Timeline = () => {
             </motion.p>
           </div>
           <div className="w-full flex flex-col items-center justify-center gap-3">
-            <div className="flex items-end justify-center -translate-x-16 gap-20 w-full">
+            {/* DESKTOP */}
+            <div className="hidden tab:flex items-end justify-center -translate-x-16 gap-20 w-full">
               <div className="flex items-start justify-start tab:items-end tab:justify-end flex-col gap-2">
-                <h2 className="text-sm font-bold text-primary md:text-lg tab:text-2xl tab:text-right">
+                <h2 className="text-sm font-bold text-primary md:text-lg lg:text-2xl tab:text-right">
                   Hackathon Announcement
                 </h2>
                 <p className="text-sm font-normal tab:text-right">
@@ -48,9 +49,34 @@ const Timeline = () => {
                   1
                 </span>
               </div>
-              <p className="flex items-start justify-start text-sm font-bold text-primary md:text-lg tab:text-2xl">
+              <p className="flex items-start justify-start text-sm font-bold text-primary md:text-lg lg:text-2xl">
                 November 18, 2023
               </p>
+            </div>
+            {/* MOBILE */}
+            <div className="tab:hidden flex items-start justify-start w-full gap-4">
+              <div className="flex flex-col items-center justify-center gap-1">
+                <img
+                  src="/icon/lineIcon.svg"
+                  alt="line_icon"
+                  className="h-[115px] tab:h-[137px] object-contain"
+                />
+                <span className="w-5 h-5 flex items-center justify-center rounded-full bg-btnlinear text-sm font-bold md:text-lg">
+                  1
+                </span>
+              </div>
+              <div className="flex items-start justify-start flex-col gap-2">
+                <h2 className="text-sm font-bold text-primary sm:text-lg">
+                  Hackathon Announcement
+                </h2>
+                <p className="text-sm font-normal">
+                  The getlinked tech hackathon 1.0 is formally announced to the
+                  general public and teams begin to get ready to register
+                </p>
+                <p className="flex items-start justify-start text-sm font-bold text-primary sm:text-lg">
+                  November 18, 2023
+                </p>
+              </div>
             </div>
           </div>
         </div>
