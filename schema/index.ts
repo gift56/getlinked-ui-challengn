c3ab5653aaa9f8013ae93780155cc32ea93f03c7
gift56 <efeasiughu@gmail.com>
2023-09-19 21:17:27 -0400

    working on contact page

--- schema/index.ts
+++ schema/index.ts
@@ -12,3 +12,16 @@ export const registerSchema = yup.object().shape({
     .oneOf([true], "Terms and condition is Required!")
     .required("Terms and condition is Required!"),
 });
+
+export const contactSchema = yup.object().shape({
+  first_name: yup.string().required("Required"),
+  email: yup.string().email("Invalid Email Address").required("Required"),
+  phone_number: yup
+    .string()
+    .matches(
+      /^(?:\+?\d{1,4}[-.\s]?)?(?:\(\d{1,4}\)[-.\s]?)?\d{1,15}$/g,
+      "Invalid phone number"
+    )
+    .required("Required"),
+  message: yup.string().required("Required"),
+});
