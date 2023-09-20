a8a1657b5e24e4c326c7917c78af41518eb73ab7
gift56 <efeasiughu@gmail.com>
2023-09-19 21:36:50 -0400

    working on contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -65,6 +65,47 @@ const Contactpage = () => {
           content="Participate in getlinked tech Hackathon 2023 stand a chance to win a Big prize"
         />
       </Head>
+      <header className="w-full py-10">
+        <div className="container">
+          <div className="flex items-center justify-between w-full">
+            <Link
+              href="/"
+              className="text-xl md:text-2xl lg:text-3xl font-clash font-bold text-white"
+            >
+              get
+              <span className="text-primary font-bold font-clash">linked</span>
+            </Link>
+            <nav className="hidden md:flex items-center justify-end gap-8">
+              {navLinks.map((item, i) => (
+                <Link
+                  href={item.href}
+                  key={i}
+                  className="text-base font-medium hover:text-primary transition-all"
+                >
+                  {item.text}
+                </Link>
+              ))}
+              <Link
+                href="/contact"
+                className="text-base font-medium bg-clip-text text-transparent bg-textgradient transition-all"
+              >
+                Contact
+              </Link>
+
+              <Link href="/register" className="w-fit">
+                <CustomizeButton
+                  text={<span className="relative">Register</span>}
+                  className="bg-btnlinear border-none rounded !px-10 !py-3 ml-10"
+                  type="button"
+                />
+              </Link>
+            </nav>
+            <h2 className="tab:hidden text-lg font-semibold font-clash text-primary">
+              Register
+            </h2>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
