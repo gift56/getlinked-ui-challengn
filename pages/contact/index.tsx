442a3624544c373648fdc6b9a3aa925f031e144d
gift56 <efeasiughu@gmail.com>
2023-09-19 21:27:04 -0400

    working on contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+import { CustomizeButton, CustomizeInput, SuccessfulModal } from "@/components";
+import Head from "next/head";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { useFormik } from "formik";
+import { ContactValue } from "@/types";
+
 const Contactpage = () => {
+  const [success, setSuccess] = useState(false);
+  const navLinks = [
+    {
+      href: "/#timeline",
+      text: "Timeline",
+    },
+    {
+      href: "/#overview",
+      text: "Overview",
+    },
+    {
+      href: "/#faqs",
+      text: "FAQs",
+    },
+  ];
+
   return <div>Contactpage</div>;
 };
 
