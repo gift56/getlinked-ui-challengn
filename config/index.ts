ddf1c9223dbe87da56fa2ae19e12cc80179b3752
gift56 <efeasiughu@gmail.com>
2023-09-19 21:18:23 -0400

    working on contact page

---
+++ config/index.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+export const baseUrl = axios.create({
+    baseURL: "https://backend.getlinked.ai",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
